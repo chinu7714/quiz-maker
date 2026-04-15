f3fef88b50470378bdb994e370c3d3a27475aea6
Chinmay Deshmukh <deshmukhchinmay7714@gmail.com>
2026-04-15 17:03:07 +0530

    Fix NextAuth adapter issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { Role } from "@prisma/client";
 import { type NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
